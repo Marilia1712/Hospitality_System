be69f9317d5744fef546382a6e7a7833e2128239
Marilia Merendi <mariliamerendi@gmail.com>
2026-05-21 16:27:06 +0200

    small fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.waiterstudy"
+        applicationId = "com.example.waiterstudy.top"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
